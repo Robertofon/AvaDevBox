a3ddd5682284cf7530794c50f39d8e8acb81f00d
Robert Köpferl <rk@flinkebits.de>
2020-08-24 01:01:42 +0200

    led mit ani

--- AvaDevBox/Controls/LedIndicator.cs
+++ AvaDevBox/Controls/LedIndicator.cs
@@ -6,15 +6,31 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
+using Avalonia.Data;
+using Avalonia.Media;
+using Color = System.Drawing.Color;
 
 namespace AvaDevBox.Controls
 {
     public class LedIndicator : TemplatedControl
     {
         public static readonly DirectProperty<LedIndicator, bool?> IsCheckedProperty =
-            ToggleButton.IsCheckedProperty.AddOwner<LedIndicator>(o=>o.IsChecked, (o,v) => o.IsChecked = v);
+            ToggleButton.IsCheckedProperty.AddOwner<LedIndicator>(o => o.IsChecked, (o,v) => o.IsChecked = v);
+
+        public static readonly DirectProperty<LedIndicator, IBrush> OnColorProperty =
+            AvaloniaProperty.RegisterDirect<LedIndicator, IBrush>(nameof(OnColor), o => o.OnColor, (o, v) => o.OnColor = v, Brushes.Transparent,
+                BindingMode.Default);
 
         private bool? _isChecked;
+        private IBrush _onColor;
+
+        static LedIndicator()
+        {
+            PseudoClass<LedIndicator, bool?>(IsCheckedProperty, c => c == true, ":checked");
+            PseudoClass<LedIndicator, bool?>(IsCheckedProperty, c => c == false, ":unchecked");
+            PseudoClass<LedIndicator, bool?>(IsCheckedProperty, c => c == null, ":indeterminate");
+        }
+
 
         public bool? IsChecked
         {
@@ -22,5 +38,10 @@ namespace AvaDevBox.Controls
             set { SetAndRaise(IsCheckedProperty, ref _isChecked, value); }
         }
 
+        public IBrush OnColor
+        {
+            get => _onColor;
+            set => SetAndRaise(OnColorProperty, ref _onColor, value);
+        }
     }
 }
