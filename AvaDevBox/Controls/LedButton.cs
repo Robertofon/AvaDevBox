028f881a5c1bface8b156bdd76f98ad10f48c31b
Robert Köpferl <rk@flinkebits.de>
2020-08-24 01:48:29 +0200

    LED Button geht auch

--- AvaDevBox/Controls/LedButton.cs
+++ AvaDevBox/Controls/LedButton.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Media;
 
 namespace AvaDevBox.Controls
 {
-    class LedButton : Button
+    public class LedButton : ToggleButton
     {
+        public static readonly StyledProperty<IBrush> OnColorProperty =
+            LedIndicator.OnColorProperty.AddOwner<LedButton>();
+
+        static LedButton()
+        {
+
+        }
+
+        public IBrush OnColor
+        {
+            get => GetValue(OnColorProperty);
+            set => SetValue(OnColorProperty, value);
+        }
     }
 }
--- AvaDevBox/Controls/LedIndicator.cs
+++ AvaDevBox/Controls/LedIndicator.cs
@@ -17,12 +17,10 @@ namespace AvaDevBox.Controls
         public static readonly DirectProperty<LedIndicator, bool?> IsCheckedProperty =
             ToggleButton.IsCheckedProperty.AddOwner<LedIndicator>(o => o.IsChecked, (o,v) => o.IsChecked = v);
 
-        public static readonly DirectProperty<LedIndicator, IBrush> OnColorProperty =
-            AvaloniaProperty.RegisterDirect<LedIndicator, IBrush>(nameof(OnColor), o => o.OnColor, (o, v) => o.OnColor = v, Brushes.Transparent,
-                BindingMode.Default);
+        public static readonly StyledProperty<IBrush> OnColorProperty =
+            AvaloniaProperty.Register<LedIndicator, IBrush>(nameof(OnColor), Brushes.Transparent, false, BindingMode.Default);
 
         private bool? _isChecked;
-        private IBrush _onColor;
 
         static LedIndicator()
         {
@@ -40,8 +38,8 @@ namespace AvaDevBox.Controls
 
         public IBrush OnColor
         {
-            get => _onColor;
-            set => SetAndRaise(OnColorProperty, ref _onColor, value);
+            get => GetValue(OnColorProperty);
+            set => SetValue(OnColorProperty, value);
         }
     }
 }
