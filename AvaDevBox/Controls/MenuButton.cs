527143353dd8c5760a2fdee881702377e2494414
Robert Köpferl <rk@flinkebits.de>
2020-08-25 18:06:47 +0200

    menubuttn begin, aurfäum

--- AvaDevBox/Controls/MenuButton.cs
+++ AvaDevBox/Controls/MenuButton.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Avalonia.Controls;
 
 namespace AvaDevBox.Controls
 {
-    class MenuButton
+    public class MenuButton : Button
     {
     }
 }
---
+++ AvaloniaControls/Demo/RatingControlDemo.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaControls.Demo
+{
+    public class RatingControlDemo : UserControl
+    {
+        public RatingControlDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
