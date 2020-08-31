eb3eb8f64db39338ebe420463039d9c1d5ad5644
Robert Köpferl <rk@flinkebits.de>
2020-08-31 12:41:03 +0200

    NotificationBar added

---
+++ AvaDevBox/Controls/NotificationBar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls.Primitives;
+
+namespace AvaDevBox.Controls
+{
+    /// <summary>
+    /// Represents a bar that can show up or disappear and show a message.
+    /// It would usually be docked to the top or bottom using a dock panel with
+    /// the actual content.
+    /// 
+    /// </summary>
+    public class NotificationBar : HeaderedContentControl
+    {
+        public bool IsOpen { get; set; }
+    }
+}
