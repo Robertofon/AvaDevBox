3068ef0f66b4784ef14fea74e0b1386b01e7b90e
Robert Köpferl <rk@flinkebits.de>
2020-10-02 10:09:57 +0200

    BusyIndicator beginnings

---
+++ Src/AvaDevBox/Controls/BusyIndicator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+
+namespace AvaDevBox.Controls
+{
+    /// <summary>
+    /// This is a very simple Control. You place it anywhere
+    /// when the property <see cref="IsActive"/> is set to true the busy
+    /// indicator spins or wobbles (depending on the theme).
+    /// There are no interactions.
+    /// </summary>
+    public class BusyIndicator : TemplatedControl
+    {
+        /// <summary>
+        /// Defines the <see cref="IsActive"/> property.
+        /// </summary>
+        public static readonly StyledProperty<bool> IsActiveProperty =
+            AvaloniaProperty.Register<BusyIndicator, bool>(nameof(IsActive), true);
+
+        /// <summary>
+        /// Indicates whether the Busy Indicator is spinning or anyways active.
+        /// </summary>
+        public bool IsActive
+        {
+            get => GetValue(IsActiveProperty);
+            set => SetValue(IsActiveProperty, value);
+        }
+
+        public BusyIndicator()
+        {
+            
+            
+        }
+    }
+}
---
+++ Src/AvaDevBox/Controls/Shapes/SevenSegmentShape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+
+namespace AvaDevBox.Controls.Shapes
+{
+    /// <summary>
+    /// Shall become a seven (or eight) segment shape where Input is one
+    /// byte. Each bit is one segment and the segments show up in the
+    /// activated color and their shape is implemented in here.
+    /// </summary>
+    class SevenSegmentShape : Shape
+    {
+        protected override Geometry CreateDefiningGeometry()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
